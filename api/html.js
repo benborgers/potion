417bf32c9a4aaa89dc48b41d4b3284fac2ca73cb
Ben Borgers <borgersbenjamin@gmail.com>
2020-08-04 13:27:13 -0400

    Add quotes around iFrame src in /html

--- api/html.js
+++ api/html.js
@@ -141,9 +141,9 @@ module.exports = async (req, res) => {
       const equationHtml = katex.renderToString(equation, { throwOnError: false })
       html.push(`<div class="equation">${equationHtml}</div>`)
     } else if(["embed"].includes(type)) {
-      html.push(`<iframe src=${block.properties.source[0][0]}></iframe>`)
+      html.push(`<iframe src="${block.properties.source[0][0]}"></iframe>`)
     } else if(["video"].includes(type)) {
-      html.push(`<iframe src=${block.format.display_source}></iframe>`)
+      html.push(`<iframe src="${block.format.display_source}"></iframe>`)
     } else {
       /* Catch blocks without handler method */
       console.log(`Unhandled block type "${block.type}"`, block)
